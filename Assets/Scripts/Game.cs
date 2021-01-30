cb49bffab5a9dd5aca8138979d33ca4016d11701
Austin Huebner <codexscript1@gmail.com>
2021-01-29 22:58:16 -0500

    Networked players

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,17 +1,27 @@
+using Cinemachine;
+using Photon.Pun;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    [SerializeField] private CinemachineVirtualCamera m_CinemachineVirtualCamera;
+    [SerializeField] private Camera m_Camera;
+
     private GameState CurrentState { get; set; }
 
     public void JoinRoom()
     {
         CurrentState = GameState.Setup;
+        
+        var fighter = PhotonNetwork.Instantiate("PlayerPrefab Austin", Vector2.zero, Quaternion.identity);
+        m_CinemachineVirtualCamera.Follow = fighter.transform;
+        m_Camera.transform.position = Vector3.zero;
     }
 
     public void LeaveRoom()
     {
         CurrentState = GameState.Lobby;
+        m_Camera.transform.position = Vector3.zero;
     }
     
     public enum GameState
---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+
+public class NetworkPlayer : MonoBehaviour
+{
+    private GhostMovement m_GhostMovement;
+
+    private GhostMovement GhostMovement
+    {
+        get
+        {
+            if (m_GhostMovement == null)
+                m_GhostMovement = GetComponent<GhostMovement>();
+
+            return m_GhostMovement;
+        }
+    }
+    
+    private PhotonView m_PhotonView;
+
+    private PhotonView PhotonView
+    {
+        get
+        {
+            if (m_PhotonView == null)
+                m_PhotonView = GetComponent<PhotonView>();
+
+            return m_PhotonView;
+        }
+    }
+    
+    private bool IsLocal => PhotonView.IsMine;
+
+    private void Awake()
+    {
+        GhostMovement.enabled = IsLocal;
+    }
+}
