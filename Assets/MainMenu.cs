b852713514e1658a077f0454fc5e48d12df1a05a
Austin Huebner <codexscript1@gmail.com>
2021-01-29 20:23:23 -0500

    Fixed issues with joining Added player count

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -11,13 +11,16 @@ public class MainMenu : MonoBehaviourPunCallbacks
     [SerializeField] private TMP_InputField m_RoomCodeField;
     [SerializeField] private Button m_CreateButton;
     [SerializeField] private Button m_JoinButton;
-    [SerializeField] private TMP_Text m_RoomCode;
     [SerializeField] private TMP_Text m_Error;
+    [SerializeField] private TMP_Text m_RoomCode;
+    [SerializeField] private TMP_Text m_PlayerCount;
     [SerializeField] private GameObject m_JoinOrCreateRoom;
 
     private string Username { get; set; }
 
     private Observable<string> RoomCode { get; } = new Observable<string>();
+
+    private Observable<int> PlayerCount { get; } = new Observable<int>();
     
     private static Random Random { get; } = new Random();
     
@@ -37,6 +40,11 @@ public class MainMenu : MonoBehaviourPunCallbacks
             m_RoomCode.text = newRoomName;
         });
         
+        PlayerCount.OnChange.AddListener(delegate(int oldInt, int newInt)
+        {
+            m_PlayerCount.text = $"Player Count: {newInt}";
+        });
+        
         PhotonNetwork.ConnectUsingSettings();
 
         void CreateRoom()
@@ -52,7 +60,8 @@ public class MainMenu : MonoBehaviourPunCallbacks
                 m_Error.text = "No room code has been entered!";
                 return;
             }
-                
+            
+            RoomCode.Value = m_RoomCodeField.text;
             PhotonNetwork.JoinRoom(m_RoomCodeField.text);
         }
     }
@@ -73,10 +82,19 @@ public class MainMenu : MonoBehaviourPunCallbacks
         m_Error.text = $"Failed to join room {m_RoomCodeField.text}!";
     }
 
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log($"Player {newPlayer.ActorNumber} joined!");
+
+        base.OnPlayerEnteredRoom(newPlayer);
+        PlayerCount.Value = PhotonNetwork.PlayerList.Length;
+    }
+
     public override void OnJoinedRoom()
     {
-        RoomCode.Value = m_RoomCodeField.text;
+        PlayerCount.Value = PhotonNetwork.PlayerList.Length;
         m_JoinOrCreateRoom.gameObject.SetActive(false);
+        Debug.Log($"Joined room {RoomCode.Value}");
     }
     
     private static string RandomString(int length)
