66c6a7d5c41c50a0ba8544e181f4b35080b398bf
Austin Huebner <codexscript1@gmail.com>
2021-01-30 12:40:01 -0500

    Added player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Player : MonoBehaviour
+    {
+        private GhostMovement m_GhostMovement;
+
+        private GhostMovement GhostMovement
+        {
+            get
+            {
+                if (m_GhostMovement == null)
+                    m_GhostMovement = GetComponent<GhostMovement>();
+
+                return m_GhostMovement;
+            }
+        }
+        
+        private HumanMovement m_HumanMovement;
+
+        private HumanMovement HumanMovement
+        {
+            get
+            {
+                if (m_HumanMovement == null)
+                    m_HumanMovement = GetComponent<HumanMovement>();
+
+                return m_HumanMovement;
+            }
+        }
+        
+        public void MakeIntoGhost()
+        {
+            
+        }
+
+        public void MakeIntoPlayer()
+        {
+            
+        }
+
+    }
+}
